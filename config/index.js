38f28bd2a22a0446ffb34bdf7ac16af907fccc7a
flowesy <49629023+flowesy@users.noreply.github.com>
2022-09-15 15:15:54 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -25,9 +25,9 @@ export const config = {
       // 想要发送的人的名字
       name: "老婆0",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "",
+      id: "o2Hza5-dDsihIEFEIM1VN_WvFe2A",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "o2Hza5-dDsihIEFEIM1VN_WvFe2A",
+      useTemplateId: "QF3zHZk_-OHUhklcmg76mrWgW8fpwZu3a1Sf2TYECg8",
       // 所在省份
       province: "广东",
       // 所在城市
